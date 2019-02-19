9c91c4a036ed360fdbfc21d2ced242a972baf4a9
maxiangyu <CoderMa@Outlook.com>
2019-02-19 23:58:06 +0800

    springboot创建多模块项目gradle构建

---
+++ basic-base/src/main/java/pers/tik/BasicBaseApplication.java
@@ -0,0 +1,13 @@
+package pers.tik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBaseApplication.class, args);
+    }
+
+}
---
+++ views-demo/src/main/java/pers/tik/ViewsDemoApplication.java
@@ -0,0 +1,13 @@
+package pers.tik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewsDemoApplication.class, args);
+    }
+
+}
